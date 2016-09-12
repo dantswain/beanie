e5f78fd25321bf4898708aae425370970583d1af
Dan Swain <dan.t.swain@gmail.com>
2016-09-11 20:01:33 -0400

    Add manifest fetch, creation check

--- lib/beanie/registry_api.ex
+++ lib/beanie/registry_api.ex
@@ -8,4 +8,23 @@ defmodule Beanie.RegistryAPI do
   def tag_list(registry, repository) do
     Registry.get(registry, [repository, "tags", "list"])
   end
+
+  def manifest(registry, repository, tag) do
+    Registry.get(registry, [repository, "manifests", tag])
+  end
+
+  def created_at_from_manifest(manifest) do
+    manifest["history"]
+    |> Enum.map(fn(h) ->
+      h["v1Compatibility"]
+      |> Poison.decode!
+      |> Map.get("created")
+      |> NaiveDateTime.from_iso8601!
+      |> NaiveDateTime.to_erl
+    end
+    )
+    |> Enum.sort
+    |> List.last
+    |> NaiveDateTime.from_erl!
+  end
 end
